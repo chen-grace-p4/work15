0566780847bda7deaf3f694cc278380feebbc7b9
chen-grace-p4 <gchen20@stuy.edu>
2021-11-14 15:42:47 -0500

    completed

--- makefile
+++ makefile
@@ -1,1 +1,9 @@
-making
+all: work15.o
+	gcc -o work15 work15.o
+work15.o: work15.c
+	gcc -c work15.c
+run:
+	./work15
+clean:
+	rm *.o
+	rm work15
--- work15.c
+++ work15.c
@@ -1,1 +1,35 @@
-code
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+char ** parse_args(char * line) {
+   //instructions say string has no more than 5 arguments
+   char ** ret = calloc(5, sizeof(char *));
+
+   //loops through line to set ret[i] to a pointer
+   //to the first word before a space and etc.
+   int i;
+   for(i = 0; line; i++) {
+      ret[i] = strsep(&line, " ");
+   }
+
+   return ret;
+}
+
+int main() {
+   printf("Command: ");
+
+   //gets inputted command
+   char test[100];
+   fgets(test, sizeof(test)-1, stdin);
+   //adds the null at the end of a string to close it off
+   test[strlen(test)-1] = '\0';
+
+   //parses the entered string to become an array of strings
+   char ** args = parse_args(test);
+   //executes the entered command using the parsed array
+   execvp(args[0], args);
+
+   return 0;
+}
